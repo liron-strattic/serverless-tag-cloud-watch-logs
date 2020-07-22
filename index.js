578ee6b554a5cf21f6307de26f26893a3e422da8
Liron Kopinsky <liron@strattic.com>
2020-07-22 17:16:18 +0300

    - Update to handle nested stacks - Get rid of lodash reference - Update to match js standard.

--- index.js
+++ index.js
@@ -1,82 +1,117 @@
-'use strict';
-
-const _ = require('lodash');
-
-let _cloudWatchLogsService = null;
-let _cloudFormationService = null;
+'use strict'
 
 class ServerlessCloudWatchLogsTagPlugin {
-
-  get stackName() {
-    return `${this.serverless.service.service}-${this.options.stage}`;
+  cloudWatchLogsService () {
+    if (!this._cloudFormationService) {
+      this._cloudFormationService = new this.provider.sdk.CloudFormation({ region: this.region })
+    }
+    return this._cloudFormationService
   }
 
-  get logGroupService() {
+  constructor (serverless, options) {
+    this._cloudWatchLogsService = null
+    this._cloudFormationService = null
 
-    if (!_cloudWatchLogsService)
-      _cloudWatchLogsService = new this.awsService.sdk.CloudWatchLogs({ region: this.options.region });
+    this.serverless = serverless
 
-    return _cloudWatchLogsService;
-  }
+    this.provider = this.serverless.getProvider('aws')
+    this.options = options || {}
+    this.region = this.provider.getRegion()
+    this.stage = this.provider.getStage()
+    this.tags = this.serverless.service.custom.cloudWatchLogsTags
 
-  get cloudWatchLogsService() {
-
-    if (!_cloudFormationService)
-      _cloudFormationService = new this.awsService.sdk.CloudFormation({ region: this.options.region });
-
-    return _cloudFormationService;
-  }
-
-  constructor(serverless, options) {
-
-    this.options = options;
-    this.serverless = serverless;
-    this.awsService = this.serverless.getProvider('aws');
-
-    this.hooks = {
-      'after:deploy:deploy': this.execute.bind(this),
-    };
-  }
-
-  execute() {
-    return this.getStackResources()
-      .then(data => this.tagCloudWatchLogs(data))
-      .then(data => this.serverless.cli.log(JSON.stringify(data)))
-      .catch(err => this.serverless.cli.log(JSON.stringify(err)));
-  }
-
-  getStackResources(nextToken) {
-    const getData = async (acc = [], nextToken) => {
-      const data = await this.cloudWatchLogsService.listStackResources({ StackName: this.stackName, NextToken: nextToken }).promise();
-      if (data.NextToken) {
-        return getData([...acc, ...data.StackResourceSummaries], data.NextToken);
-      } else {
-        return [...acc, ...data.StackResourceSummaries];
+    const commands = {
+      deploy: {
+        lifecycleEvents: ['packaging', 'functions', 'deploy']
       }
     }
-    return getData();
+    const hooks = {
+      'after:deploy:deploy': this.execute.bind(this)
+      // 'after:deploy:function:deploy': this.execute.bind(this)
+    }
+    this.commands = commands
+    this.hooks = hooks
   }
 
-  tagCloudWatchLogs(data) {
-    const cloudWatchResources = _.filter(data, { ResourceType: 'AWS::Logs::LogGroup' });
+  execute () {
+    this.serverless.cli.log('ServerlessCloudWatchLogsTagPlugin-execute')
+    this.serverless.cli.log(JSON.stringify(this.tags))
+    const stackName = this.provider.naming.getStackName(this.stage)
+    return this.processStack(stackName)
+  }
 
-    const promises = _.map(cloudWatchResources, item => {
-      return new Promise((resolve, reject) => {
+  processStack (stackName) {
+    this.serverless.cli.log(`ProcessStack-${stackName}`)
+    return this.getStackResources(stackName)
+      .then(data => this.tagCloudWatchLogs(data))
+      .then(data => this.serverless.cli.log(JSON.stringify(data)))
+      .catch(err => this.serverless.cli.log(JSON.stringify(err)))
+  }
 
-        const params = {
+  getStackResources (stackName) {
+    this.serverless.cli.log('getStackResources')
+    this.serverless.cli.log(stackName)
+    const getData = async (acc = [], nextToken) => {
+      const data = await this.provider.request(
+        'CloudFormation',
+        'listStackResources',
+        {
+          StackName: stackName,
+          NextToken: nextToken
+        },
+        this.stage,
+        this.region
+      )
+      this.serverless.cli.log('getStackResources-data')
+      if (data.NextToken) {
+        return getData([...acc, ...data.StackResourceSummaries], data.NextToken)
+      } else {
+        return [...acc, ...data.StackResourceSummaries]
+      }
+    }
+    return getData()
+  }
+
+  processLogGroup (item) {
+    return new Promise((resolve, reject) => {
+      this.serverless.cli.log(`Tag LogGroup Begin ${item.LogicalResourceId}`)
+      this.provider.request(
+        'CloudWatchLogs',
+        'tagLogGroup',
+        {
           logGroupName: item.PhysicalResourceId,
-          tags: this.serverless.service.custom.cloudWatchLogsTags
-        };
+          tags: this.tags
+        },
+        this.stage,
+        this.region
+      ).then(() => {
+        this.serverless.cli.log(`Tagged LogGroup ${item.LogicalResourceId}`)
+        resolve(`Tagged LogGroup ${item.LogicalResourceId}`)
+      }).catch((err) => {
+        this.serverless.cli.log(`Tagged LogGroup ${item.LogicalResourceId} - Error`)
+        this.serverless.cli.log(err)
+        reject(err)
+      })
+    })
+  }
 
-        this.logGroupService.tagLogGroup(params, (err, apiData) => {
-          if (err) return reject(err);
-          resolve(`Tagged LogGroup ${item.LogicalResourceId}`);
-        });
-      });
-    });
+  tagCloudWatchLogs (data) {
+    this.serverless.cli.log('tagCloudWatchLogs')
+    // Handle nested stacks recursively
+    const nestedStackResources = data.filter(d => d.ResourceType === 'AWS::CloudFormation::Stack')
+    this.serverless.cli.log(JSON.stringify(nestedStackResources))
+    const nestedStackPromises = nestedStackResources.map(item => this.processStack(item.PhysicalResourceId))
 
-    return Promise.all(promises);
+    // Handle all log groups
+    const cloudWatchResources = data.filter(d => d.ResourceType === 'AWS::Logs::LogGroup')
+    this.serverless.cli.log(JSON.stringify(cloudWatchResources))
+    const promises = cloudWatchResources.map(item => this.processLogGroup(item))
+
+    const allPromises = nestedStackPromises.concat(promises)
+
+    this.serverless.cli.log(`Updating logs in ${promises.length} logGroups and ${nestedStackPromises.length} nested stacks`)
+    return Promise.all(allPromises)
   }
 }
 
-module.exports = ServerlessCloudWatchLogsTagPlugin;
+module.exports = ServerlessCloudWatchLogsTagPlugin
